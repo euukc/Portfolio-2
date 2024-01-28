78f536aab8b359f9563ca5ad96bddd2f52207f88
euukc <56362484+euukc@users.noreply.github.com>
2024-01-28 13:38:30 -0300

    adicionando evento (onclick) no menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,21 @@
-/* SLIDES */
+// Scroll para a Section
 
-var swiper = new Swiper (".swiper". {
-    cssMode: true,
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",  
-    },
-    pagination: {
-        el: ".swiper-pagination",
-    },
-    keyboard: true,
+document.addEventListener('DOMContentLoaded', function() {
+    const menu = document.getElementById('cabecalho-menu');
+    const itensMenu = menu.querySelectorAll('#cabecalho-menu-itens');
+    
+
+    itensMenu.forEach((item) => {
+        item.addEventListener('click', () => {
+            const targetId = item.getAttribute('data-target');
+            const targetSection = document.getElementById(targetId);
+            
+            if (targetSection) {
+                targetSection.scrollIntoView({ behavior: 'smooth', block: 'center' });
+            }
+        });
+    });
 });
+
+
+
